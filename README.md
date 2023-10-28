4949096d7f94463156c75b29d3c6951b88313fb6
guoxian.rex <guoxian.rex@bytedance.com>
2023-10-28 17:11:12 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PegasusG-Cover-Maker
 PegasusG Cover Maker
+
+
+marquee没有的话，有cartridge和screenshot和Texture
---
+++ main.py
@@ -0,0 +1,75 @@
+import os
+import PIL
+from PIL import Image
+
+
+leftImgName = 'boxFront.png'
+topImgName = 'boxFront.png'
+bottomImgNames = ['marquee.png', 'cartridge.png', 'screenshot.png', 'Texture.png']
+
+files = ['a', 'b']
+
+
+inputFolderName = 'input'
+
+
+def output(topImgPath, bottomImgPath):
+    print(topImgPath + ' ' + bottomImgPath)
+
+    new_im = Image.new('RGBA', (640, 633))
+    cover = Image.open(os.path.join('', leftImgName))
+
+    topImg = Image.open(topImgPath)
+    topImg.thumbnail((640, 316))
+    new_im.paste(topImg, (132, 0))
+
+    bottomImg = Image.open(bottomImgPath)
+    bottomImg.thumbnail((640, 316))
+    new_im.paste(bottomImg, (132, 317))
+
+    new_im.paste(cover, (0,0), cover)
+
+    new_im.save(folder + '_final.png')
+
+
+for filename in os.listdir(inputFolderName):
+    folder = os.path.join(inputFolderName, filename)
+
+    if os.path.isdir(folder):
+        topImgPath = ''
+        bottomImgPath = ''
+        for imgName in os.listdir(folder):
+            imgPath = os.path.join(folder, imgName)
+            if os.path.isfile(imgPath):
+                # print(imgPath)
+                if (imgName == topImgName):
+                    topImgPath = imgPath
+              
+        for cand in bottomImgNames:
+            find = False 
+            for imgName in os.listdir(folder):
+                if (imgName == cand):
+                    find = True 
+                    print(cand, imgName)
+                    imgPath = os.path.join(folder, imgName)
+                    if os.path.isfile(imgPath):
+                        bottomImgPath = imgPath
+                        # print(cand)
+                        if(topImgPath != ""):
+                            output(topImgPath, bottomImgPath)
+                            break
+                        # else:
+                        #     continue
+                        break
+                    break
+            if find: 
+                break 
+
+
+                     
+
+
+
+
+exit()
+
